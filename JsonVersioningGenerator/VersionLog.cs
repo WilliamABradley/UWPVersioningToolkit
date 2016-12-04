c2a1bcf9ccdea805bf8dd5a58387a980a5171167
William Bradley <WilliamABradley@outlook.com>
2016-12-05 04:41:52 +1300

    Initial Commit (Created 02/04/2016). Reversioned SDK to 14393.

---
+++ JsonVersioningGenerator/VersionLog.cs
@@ -0,0 +1,11 @@
+﻿namespace JsonVersioningGenerator
+{
+    public class VersionLog
+    {
+        public uint Major { get; set; }
+        public uint Minor { get; set; }
+        public uint Build { get; set; }
+        public uint Revision { get; set; }
+        public string log { get; set; }
+    }
+}
